10b074f0c1ab51893b4606695430520a6778ecb2
zannat20040 <shanzidadisha1234@gmail.com>
2024-11-09 04:29:54 +0600

    Schema change

--- model/healthModel.js
+++ model/healthModel.js
@@ -3,27 +3,27 @@ const mongoose = require("mongoose");
 const healthSchema = new mongoose.Schema({
   patientId: {
     type: String,
-    required: true, // Make sure this field is always provided
+    required: true, // Ensure this field is always provided
   },
-  email: {
+  deviceEmail: {
     type: String,
-    required: true, // Make sure this field is always provided
+    required: true, // Ensure this field is always provided
   },
   temperature: {
     type: Number,
     default: 98.6, // Default body temperature in Fahrenheit
   },
-  spo2: { 
-    type: Number, 
-    default: 95, // Oxygen Saturation (SpO₂) in percentage 
+  spo2: {
+    type: Number,
+    default: 95, // Oxygen Saturation (SpO₂) in percentage
   },
-  pulseRate: { 
-    type: Number, 
+  pulseRate: {
+    type: Number,
     default: 75, // Pulse Rate (Heart Rate) in beats per minute (BPM)
   },
-  rawPulseSignal: { 
-    type: [Number], 
-    default: [] // Raw Pulse Signal (for visualization or analysis)
+  rawPulseSignal: {
+    type: [Number],
+    default: [], // Raw Pulse Signal (for visualization or analysis)
   },
   timestamp: {
     type: Date,
